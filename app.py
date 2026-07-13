9e44f7e38494f593facc648712a2bff952025d45
NP <naveenprasaath29@gmail.com>
2026-07-14 00:50:05 +0530

    Added K8s files

---
+++ app.py
@@ -0,0 +1,13 @@
+# pyrefly: ignore [missing-import]
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    message = "Hi This is the Webapp running on Nodeport IP in pod"
+    return f"<h1>{message}</h1>\n<p>Pod ID: {os.environ.get('HOSTNAME', 'localhost')}</p>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
