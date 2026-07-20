a428a88dede49db28a52f226955657d782b202c4
NAVEEN PRASAATH <67555446+NAVEENPRASAATH23@users.noreply.github.com>
2026-07-20 23:42:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    message = "Hi This is the Webapp running on Nodeport IP in pod"
+    message = "Hi This is the Webapp running on Nodeport IP in pod for the testing purpose"
     return f"<h1>{message}</h1>\n<p>Pod ID: {os.environ.get('HOSTNAME', 'localhost')}</p>"
 
 if __name__ == '__main__':
